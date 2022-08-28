d0aeb542b4977b05c794b6c652128fd98edfe86f
Gulshan Nadaph <gulshanaranadaph@gmail.com>
2022-08-29 01:18:46 +0530

    add draft test

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,5 +1,69 @@
-describe('empty spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
-})
+/// <reference types="cypress"/>
+describe("wave-trial login", () => {
+  beforeEach(() => {
+    cy.visit("https://wave-trial.getbynder.com/login/");
+  });
+
+  it("verifies login and logout functionality", () => {
+    cy.intercept("POST", "**/user/doLogin/").as("login");
+    cy.get("#inputEmail")
+      .type("gul@mailinator.com")
+      .then(() => {
+        cy.get("#inputPassword")
+          .type("Tester@123")
+          .then(() => {
+            cy.get(".login-btn")
+              .click()
+              .then(() => {
+                cy.wait("@login")
+                  .its("response.statusCode")
+                  .should("eq", 302)
+                  .then(() => {
+                    cy.url().should(
+                      "be.equal",
+                      "https://wave-trial.getbynder.com/account/dashboard/"
+                    );
+                    cy.title().should("eq", "Wave trial");
+                  })
+                  .then(() => {
+                    cy.get(".profile")
+                      .click()
+                      .then(() => {
+                        cy.contains("Logout")
+                          .click()
+                          .then(() => {
+                            cy.url().should(
+                              "be.equal",
+                              "https://wave-trial.getbynder.com/login/"
+                            );
+                            cy.title().should("eq", "Wave trial");
+                          });
+                      });
+                  });
+              });
+          });
+      });
+  });
+
+  it("verifies invalid login flow", () => {
+    const runout = [
+      "Security Check",
+      "You have entered an incorrect username or password.",
+    ];
+    const regex = new RegExp(`${runout.join("|")}`, "g");
+    cy.intercept("POST", "**/user/doLogin/").as("login");
+    cy.get("#inputEmail")
+      .type("gul1@mailinator.com")
+      .then(() => {
+        cy.get("#inputPassword")
+          .type("Tester@123")
+          .then(() => {
+            cy.get(".login-btn")
+              .click()
+              .then(() => {
+                cy.get("#login").contains(regex);
+              });
+          });
+      });
+  });
+});
